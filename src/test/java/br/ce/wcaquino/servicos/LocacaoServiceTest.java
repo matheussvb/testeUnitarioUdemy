902f25c38d68c2fdd78069c9f317551b1852f1df
Matheus <matheus.svb@hotmail.com>
2021-01-04 11:23:51 -0300

    assertThat e ErrorCollector

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -4,17 +4,27 @@ import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
 import br.ce.wcaquino.utils.DataUtils;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+
+import javax.xml.crypto.Data;
 
 import java.util.Date;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static br.ce.wcaquino.utils.DataUtils.isMesmaData;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class LocacaoServiceTest {
 
+    @Rule
+    public ErrorCollector error = new ErrorCollector();
+
+
     @Test
-    public void teste() {
+    public void testeLocacao() {
         //cenario
         LocacaoService locacaoService = new LocacaoService();
         Usuario usuario = new Usuario();
@@ -24,8 +34,13 @@ public class LocacaoServiceTest {
         Locacao locacao = locacaoService.alugarFilme(usuario, filme);
 
         //verificacao
-        assertEquals(5.0, locacao.getValor(), 0.01);
-        assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-        assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+//        assertEquals(5.0, locacao.getValor(), 0.01);
+//        assertThat(locacao.getValor(), is(equalTo(5.0)));
+        assertThat(locacao.getValor(), is(equalTo(5.0)));
+        error.checkThat(locacao.getValor(), is(equalTo(5.0)));
+        error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+//        assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+//        assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+
     }
 }
